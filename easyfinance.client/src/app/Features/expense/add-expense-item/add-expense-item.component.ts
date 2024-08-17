6480b016aed41c8185441ff4fbc4c7622666423d
Felipe Soares <>
2024-08-17 14:53:44 +0100

    Feat: Created expense item

---
+++ easyfinance.client/src/app/Features/expense/add-expense-item/add-expense-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-expense-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-expense-item.component.html',
+  styleUrl: './add-expense-item.component.css'
+})
+export class AddExpenseItemComponent {
+
+}
---
+++ easyfinance.client/src/app/Features/expense/list-expense-items/list-expense-items.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-expense-items',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-expense-items.component.html',
+  styleUrl: './list-expense-items.component.css'
+})
+export class ListExpenseItemsComponent {
+
+}
