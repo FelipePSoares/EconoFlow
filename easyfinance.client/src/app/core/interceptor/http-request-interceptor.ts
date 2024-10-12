510ee1e0c083205898ed7e44df78b51755e4592d
Felipe Soares <>
2024-10-12 22:16:14 +0200

    Feat: Show backend errors and new design.

--- easyfinance.client/src/app/core/interceptor/http-request-interceptor.ts
+++ easyfinance.client/src/app/core/interceptor/http-request-interceptor.ts
@@ -17,17 +17,21 @@ export const HttpRequestInterceptor: HttpInterceptorFn = (req, next) => {
 }
 
 function handleAuthError(err: HttpErrorResponse, router: Router, authService: AuthService): Observable<any> {
-  if ((err.status === 401 || err.status === 403) && !err.url?.includes('logout')) {
+  if ((err.status === 401 || err.status === 403) && !err.url?.includes('logout') && !err.url?.includes('login')) {
     console.log('authInterceptor 401 or 403');
-    authService.signOut();
+    try { authService.signOut().subscribe(); } catch (e) { } // silent catch to avoid stop the redirection
     router.navigate(['login']);
 
     return of(err.message);
   }
-  let apiErrorResponse: ApiErrorResponse = <ApiErrorResponse>{};
+  let apiErrorResponse: ApiErrorResponse = <ApiErrorResponse>{ errors: {} };
 
-  if (err.error) {
+  if (err.error?.errors) {
     apiErrorResponse = err.error as ApiErrorResponse;
+  } else if (err.status === 401 && err.url?.includes('login') && err.error?.detail === 'LockedOut') {
+    apiErrorResponse.errors['general'] = 'User blocked!';
+  } else if (err.status === 401 && err.url?.includes('login')) {
+    apiErrorResponse.errors['general'] = 'Incorrect login information!';
   } else {
     apiErrorResponse.errors['general'] = 'An unexpected error occurs, try again...';
   }
