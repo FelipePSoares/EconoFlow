3ac851fb1d592b03bd704cc5a6d80c326ce7c4fe
ahmedhalac <ahmedhalac@outlook.com>
2024-11-20 22:29:23 +0100

    fix(#257): Fixed failed tests

--- easyfinance.client/cypress/support/commands.ts
+++ easyfinance.client/cypress/support/commands.ts
@@ -1,11 +1,11 @@
 Cypress.Commands.add('login', (username, password) => {
     cy.session(
-        username, 
+        username,
         () => {
             cy.intercept('GET', '/api/account/').as('getAccount')
             cy.visit('/login')
-            cy.get('#username').type(username)
-            cy.get('#password').type(`${password}{enter}`, { log: false })
+            cy.get('input[formControlName=email]').type(username)
+            cy.get('input[formControlName=password]').type(`${password}{enter}`, { log: false })
         },
         {
             validate: () => {
@@ -26,9 +26,9 @@ Cypress.Commands.add('register', (username, password) => {
             cy.get('#password').type(password)
             cy.get('#confirmPassword').type(`${password}{enter}`, { log: false })
             cy.url().should('include', 'login')
-            
+
             cy.login(username, password)
-            
+
             cy.visit('/')
             cy.url().should('include', 'first-signin')
         }
