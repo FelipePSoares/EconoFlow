034e1d9f10fa2db764d763b5862f535d0e3841c5
Felipe Soares <>
2024-12-31 16:54:09 +0100

    fix

--- easyfinance.client/cypress/support/commands.ts
+++ easyfinance.client/cypress/support/commands.ts
@@ -21,18 +21,20 @@ Cypress.Commands.add('register', (username, password) => {
     cy.session(
         username,
         () => {
+            cy.intercept('GET', '/api/account/').as('getAccount')
+            cy.intercept('POST', '/api/account/register').as('postAccount')
+
             cy.visit('/register')
             cy.get('input[formControlName=email]').type(username)
             cy.get('input[formControlName=password]').type(password)
             cy.get('input[formControlName=confirmPassword]').type(`${password}{enter}`, { force: true, log: false })
+            cy.wait('@postAccount')
             cy.url().should('include', 'login')
 
-            cy.login(username, password)
+            cy.get('input[formControlName=email]').type(username)
+            cy.get('input[formControlName=password]').type(`${password}{enter}`, { log: false })
 
-            cy.visit('/')
             cy.url().should('include', 'first-signin')
-
-            cy.intercept('GET', '/api/account/').as('getAccount')
 
             cy.get('input[formControlName=firstName]').type('test')
             cy.get('input[formControlName=lastName]').type('test')
