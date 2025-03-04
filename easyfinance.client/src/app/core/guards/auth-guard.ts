3030082b2d6479f9c822fe66927f49a2b6a8ec1a
Felipe Soares <>
2025-03-04 01:02:27 +0100

    feat: redirect user already logged to defaut project when they open the application

--- easyfinance.client/src/app/core/guards/auth-guard.ts
+++ easyfinance.client/src/app/core/guards/auth-guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
 import { Observable, map } from 'rxjs';
 import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,7 @@ import { AuthService } from '../services/auth.service';
 export class AuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
+    private userService: UserService,
     private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
@@ -20,6 +22,14 @@ export class AuthGuard implements CanActivate {
       map((isSignedIn) => {
         if (!isSignedIn) {
           this.router.navigate(['login'], { queryParams: { returnUrl: state.url } });
+          return false;
+        }
+
+        if (!sessionStorage.getItem("visited")) {
+          sessionStorage.setItem("visited", "true");
+          this.userService.loggedUser$.subscribe(user => {
+            this.router.navigate(['/projects', user.defaultProjectId]);
+          });
           return false;
         }
 
