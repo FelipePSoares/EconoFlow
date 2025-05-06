e266ccff842c3b995a8f3e6962071560be3ae1e1
Felipe Soares <>
2025-05-06 12:20:02 +0200

    Feat: Created Initial class for payment

---
+++ EasyFinance.Domain/Financial/PaymentStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace EasyFinance.Domain.Financial
+{
+    public enum PaymentStatus
+    {
+        Pending = 0,
+        Paid = 1,
+        Canceled = 3
+    }
+}
---
+++ EasyFinance.Domain/Financial/PaymentType.cs
@@ -0,0 +1,8 @@
+﻿namespace EasyFinance.Domain.Financial
+{
+    public enum PaymentType
+    {
+        OneTime = 0,
+        Recurring = 1
+    }
+}
