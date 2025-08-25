d11fb24f40bc3ee167cca9e5eac87916e8e10b2b
Felipe Soares <FelipePSoares@users.noreply.github.com>
2025-08-25 13:48:53 +0200

    Feat: Added integration tests to test archive categories

--- easyfinance.client/cypress/e2e/category/add-category.cy.ts
+++ easyfinance.client/cypress/e2e/category/add-category.cy.ts
@@ -9,11 +9,6 @@ describe('EconoFlow - category add Tests', () => {
         var project = projects.defaultProject;
 
         cy.visit('/projects/' + project.id)
-
-        cy.get('.btn-add').click();
-        cy.get('app-list-categories .btn-add').click();
-
-        cy.focused().should('have.attr', 'formControlName', 'name')
       })
     })
   })
@@ -21,8 +16,15 @@ describe('EconoFlow - category add Tests', () => {
   it('should add a new category', () => {
     cy.intercept('GET', '**/categories*').as('getCategories')
     cy.intercept('POST', '**/categories*').as('postCategories')
+    cy.wait<CategoryReq, CategoryRes[]>('@getCategories');
 
     cy.fixture('categories').then((categories) => {
+      cy.get('.btn-add').click();
+      cy.wait<CategoryReq, CategoryRes[]>('@getCategories');
+      cy.get('app-list-categories .btn-add').click();
+
+      cy.focused().should('have.attr', 'formControlName', 'name')
+
       var category = categories.testGroceriesCategory;
 
       cy.get('input[formControlName=name]').type(category.name)
@@ -32,15 +34,98 @@ describe('EconoFlow - category add Tests', () => {
       cy.wait<CategoryReq, CategoryRes>('@postCategories').then(({ request, response }) => {
         expect(response?.statusCode).to.equal(201)
 
-        const incomeCreated = response?.body
+        const categoryCreated = response?.body
 
         cy.get("mat-snack-bar-container").should("be.visible").contains('Created Successfully!')
 
-        cy.wait<CategoryReq, CategoryRes[]>('@getCategories').then(response => {
-          const exists = response.response?.body.some(item => item.id == incomeCreated?.id)
+        cy.wait<CategoryReq, CategoryRes[]>('@getCategories').then(({ request, response }) => {
+          const exists = response?.body.some(item => item.id == categoryCreated?.id)
           expect(exists).to.be.true
         })
 
+      })
+    })
+  })
+
+  it('should be possible view archived categories with expenses', () => {
+    cy.intercept('GET', '**/categories*').as('getCategories')
+    cy.intercept('POST', '**/categories*').as('postCategories')
+    cy.intercept('PUT', '**/categories/**').as('putCategories')
+    cy.intercept('GET', '**/expenses*').as('getExpenses')
+    cy.intercept('POST', '**/expenses*').as('postExpenses')
+
+    cy.get('#previous').click()
+
+    cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+
+    cy.fixture('projects').then((projects) => {
+      cy.fixture('categories').then((categories) => {
+        cy.fixture('expenses').then((expenses) => {
+          cy.get('.btn-add').click();
+          cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+          cy.get('app-list-categories .btn-add').click();
+
+          var category = categories.testArchivedCategory;
+
+          cy.get('input[formControlName=name]').type(category.name)
+          cy.get('button').contains('Create').click()
+
+          cy.wait<CategoryReq, CategoryRes>('@postCategories').then(({ request, response }) => {
+            expect(response?.statusCode).to.equal(201)
+
+            const categoryCreated = response?.body
+            cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+            cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+            cy.visit('/projects/' + projects.defaultProject.id + '/categories/' + categoryCreated?.id + '/expenses')
+
+            cy.get('.btn-add').click();
+
+            var expense = expenses.testSomeExpense;
+
+            cy.get('input[formControlName=name]').type(expense.name)
+            cy.get('input[formControlName=budget]').type(expense.budget)
+            cy.get('input[formControlName=amount]').type(expense.amount)
+
+            cy.get('button').contains('Create').click();
+
+            cy.wait<ExpenseReq, ExpenseRes>('@postExpenses').then(({ request, response }) => {
+              expect(response?.statusCode).to.equal(201)
+
+              cy.visit('/projects/' + projects.defaultProject.id)
+              cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+
+              cy.get('#next').click()
+              cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+
+              cy.get('.btn-add').click()
+              cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+              cy.get('.btn-outline-danger').last().click()
+              cy.get('button').contains('Archive').click()
+
+              cy.wait<CategoryReq, CategoryRes[]>('@putCategories').then(({ request, response }) => {
+                expect(response?.statusCode).to.equal(204)
+                cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+
+                cy.get('button').contains('close').click()
+                cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
+
+                cy.wait<CategoryReq, CategoryRes[]>('@getCategories').then(({ request, response }) => {
+                  const exists = response?.body.some(item => item.id == categoryCreated?.id)
+                  expect(exists).to.be.false
+
+                  cy.get('#previous').click()
+
+                  cy.wait<CategoryReq, CategoryRes[]>('@getCategories').then(({ request, response }) => {
+                    cy.log('categoryId: ' + categoryCreated?.id)
+                    const exists2 = response?.body.some(item => item.id == categoryCreated?.id)
+                    expect(exists2).to.be.true
+                  })
+                })
+              })
+
+            })
+          })
+        })
       })
     })
   })
--- easyfinance.client/src/app/features/category/add-category/add-category.component.ts
+++ easyfinance.client/src/app/features/category/add-category/add-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
@@ -30,7 +30,7 @@ import { DefaultCategory } from '../../../core/models/default-category';
     templateUrl: './add-category.component.html',
     styleUrl: './add-category.component.css'
 })
-export class AddCategoryComponent implements OnInit {
+export class AddCategoryComponent implements OnInit, AfterViewInit {
   categoryForm!: FormGroup;
   httpErrors = false;
   errors!: Record<string, string[]>;
@@ -41,6 +41,8 @@ export class AddCategoryComponent implements OnInit {
   defaultCategories$: Observable<string[]> = this.defaultCategories.asObservable();
   
   filteredCategories$: Observable<string[]> = new Observable<string[]>();
+
+  @ViewChild('nameInput') nameInput!: ElementRef;
 
   constructor(
     private categoryService: CategoryService,
@@ -70,6 +72,10 @@ export class AddCategoryComponent implements OnInit {
     ]).pipe(
       map(([searchValue, categories]) => this.filterCategories(searchValue || '', categories))
     );
+  }
+
+  ngAfterViewInit(): void {
+    this.nameInput.nativeElement.focus();
   }
 
   private filterCategories(value: string, categories: string[]): string[] {
