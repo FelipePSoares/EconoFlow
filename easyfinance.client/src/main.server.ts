fa3a3de91597112d194a5ec61719dacefea2ff90
Felipe Soares <>
2025-08-29 14:03:33 +0200

    fix: add nonce to main.server

--- easyfinance.client/src/main.server.ts
+++ easyfinance.client/src/main.server.ts
@@ -1,11 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/features/app.component';
 import { provideServerRendering } from '@angular/platform-server';
-import { importProvidersFrom } from '@angular/core';
+import { CSP_NONCE, importProvidersFrom } from '@angular/core';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { HttpClient, provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { TranslateHttpLoader } from './app/core/utils/loaders/translate-http-loader';
 import { LoadingInterceptor } from './app/core/interceptor/loading.interceptor';
+
+function getCspNonce(): string | null {
+  const meta = document.querySelector<HTMLMetaElement>('meta[name="csp-nonce"]');
+  return meta?.content ?? null;
+}
+
+const nonce = getCspNonce();
+console.log("CSP Nonce:", nonce);
 
 const bootstrap = () => bootstrapApplication(AppComponent, {
   providers: [
@@ -23,7 +31,8 @@ const bootstrap = () => bootstrapApplication(AppComponent, {
     provideHttpClient(
       withFetch(),
       withInterceptors([LoadingInterceptor])
-    )
+    ),
+    { provide: CSP_NONCE, useValue: nonce }
   ]
 });
 
