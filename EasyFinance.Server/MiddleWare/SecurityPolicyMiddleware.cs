cf503bf599397425b3b5d926d8df1cf7c63706a7
Felipe Soares <>
2025-08-29 13:51:50 +0200

    Feat: Added nonce to meta tag

--- EasyFinance.Server/MiddleWare/SecurityPolicyMiddleware.cs
+++ EasyFinance.Server/MiddleWare/SecurityPolicyMiddleware.cs
@@ -8,21 +8,32 @@ namespace EasyFinance.Server.MiddleWare
 
         public async Task Invoke(HttpContext context)
         {
-            var bytes = RandomNumberGenerator.GetBytes(16);
-            var nonce = Convert.ToBase64String(bytes);
-            context.Items["CSP-Nonce"] = nonce;
+            if (context.Request.Path == "/" || context.Request.Path.Value!.EndsWith("index.html"))
+            {
+                var bytes = RandomNumberGenerator.GetBytes(16);
+                var nonce = Convert.ToBase64String(bytes);
+                context.Items["CSP-Nonce"] = nonce;
 
-            context.Response.Headers.Append("Content-Security-Policy",
-                "default-src 'self'; " +
-                $"script-src 'self' 'nonce-{nonce}'; " +
-                $"style-src 'self' https://fonts.googleapis.com 'nonce-{nonce}'; " +
-                "font-src 'self' https://fonts.gstatic.com; " +
-                "img-src 'self' data:; " +
-                "connect-src 'self' https://econoflow.pt; " +
-                "frame-ancestors 'none'; " +
-                "object-src 'none'; " +
-                "base-uri 'self'; " +
-                "form-action 'self';");
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html");
+                var html = await File.ReadAllTextAsync(filePath);
+
+                html = html.Replace("{{nonce}}", nonce);
+
+                context.Response.Headers.Append("Content-Security-Policy",
+                    "default-src 'self'; " +
+                    $"script-src 'self' 'nonce-{nonce}'; " +
+                    $"style-src 'self' https://fonts.googleapis.com 'nonce-{nonce}'; " +
+                    "font-src 'self' https://fonts.gstatic.com; " +
+                    "img-src 'self' data:; " +
+                    "connect-src 'self' https://econoflow.pt; " +
+                    "frame-ancestors 'none'; " +
+                    "object-src 'none'; " +
+                    "base-uri 'self'; " +
+                    "form-action 'self';");
+
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(html);
+            }
             await _next(context);
         }
     }
