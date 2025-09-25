d622f6dcdd599f5c367e89475d75fea75e18b5d1
Felipe Soares <>
2025-09-25 18:31:46 +0200

    Feat: Improved category tests

--- easyfinance.client/cypress/e2e/category/add-category.cy.ts
+++ easyfinance.client/cypress/e2e/category/add-category.cy.ts
@@ -5,12 +5,12 @@ describe('EconoFlow - category add Tests', () => {
       const user = users.testUser;
       cy.login(user.username, user.password)
 
-      cy.intercept('GET', '**/categories*').as('getCategories')
+      cy.intercept('GET', '**/projects/*/year-summary/*').as('getProjects')
       cy.fixture('projects').then((projects) => {
         var project = projects.defaultProject;
 
         cy.visit('/projects/' + project.id)
-        cy.wait<CategoryReq, CategoryRes[]>('@getCategories');
+        cy.wait<CategoryReq, CategoryRes[]>('@getProjects');
       })
     })
   })
@@ -47,78 +47,45 @@ describe('EconoFlow - category add Tests', () => {
     })
   })
 
-  it.only('should be possible view archived categories with expenses', () => {
+  it('should be possible view archived categories with expenses', () => {
     cy.intercept('GET', '**/projects/*/year-summary/*').as('getProjects')
-    cy.intercept('POST', '**/categories*').as('postCategories')
-    cy.intercept('PUT', '**/categories/**').as('putCategories')
-    cy.intercept('GET', '**/expenses*').as('getExpenses')
-    cy.intercept('POST', '**/expenses*').as('postExpenses')
-
-    cy.get('#previous').click()
+    cy.intercept('GET', '**/categories*').as('getCategories')
 
     cy.fixture('projects').then((projects) => {
       cy.fixture('categories').then((categories) => {
         cy.fixture('expenses').then((expenses) => {
-          cy.wait<ProjectReq, ProjectRes>('@getProjects').then(({ request, response }) => {
+          cy.request('POST', 'api/Projects/' + projects.defaultProject.id + '/Categories', {
+            name: categories.testArchivedCategory.name
+          }).then((resp) => {
+            expect(resp?.status).to.equal(201)
 
-            cy.get('.btn-add').click();
-            cy.get('app-list-categories .btn-add').click();
+            const archivedCategory = resp.body as CategoryRes;
+            cy.request('POST', 'api/Projects/' + projects.defaultProject.id + '/Categories/' + archivedCategory.id + '/Expenses', {
+              date: new Date(new Date().setMonth(new Date().getMonth() - 1)).toISOString().split("T")[0],
+              name: expenses.testSomeExpense.name,
+              budget: expenses.testSomeExpense.budget,
+              amount: expenses.testSomeExpense.amount
+            }).then((resp) => {
+              expect(resp?.status).to.equal(201)
 
-            var category = categories.testArchivedCategory;
-
-            cy.get('input[formControlName=name]').type(category.name)
-            cy.get('button').contains('Create').click()
-
-            cy.wait<CategoryReq, CategoryRes>('@postCategories').then(({ request, response }) => {
-              expect(response?.statusCode).to.equal(201)
-
-              const categoryCreated = response?.body
-              cy.visit('/projects/' + projects.defaultProject.id + '/categories/' + categoryCreated?.id + '/expenses')
-
-              cy.get('.btn-add').click();
-
-              var expense = expenses.testSomeExpense;
-
-              cy.get('input[formControlName=name]').type(expense.name)
-              cy.get('input[formControlName=budget]').type(expense.budget)
-              cy.get('input[formControlName=amount]').type(expense.amount)
-
-              cy.get('button').contains('Create').click();
-
-              cy.wait<ExpenseReq, ExpenseRes>('@postExpenses').then(({ request, response }) => {
-                expect(response?.statusCode).to.equal(201)
+              cy.request('PUT', 'api/Projects/' + projects.defaultProject.id + '/Categories/' + archivedCategory.id + '/Archive', {}).then((resp) => {
+                expect(resp?.status).to.equal(204)
 
                 cy.visit('/projects/' + projects.defaultProject.id)
-
-                cy.wait<ProjectReq, ProjectRes>('@getProjects')
-
-                cy.get('#next').click()
-
-                cy.get('.btn-add').click()
-                cy.get('.btn-outline-danger').last().click()
-                cy.get('button').contains('Archive').click()
-
-                cy.wait<CategoryReq, CategoryRes[]>('@putCategories').then(({ request, response }) => {
-                  expect(response?.statusCode).to.equal(204)
-
-                  cy.get('button').contains('close').click()
-                  cy.intercept('GET', '**/categories*').as('getCategories')
-                  cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
-                  cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
-
+                cy.wait<ProjectReq, ProjectRes>('@getProjects').then(({ request, response }) => {
                   cy.wait<CategoryReq, CategoryRes[]>('@getCategories').then(({ request, response }) => {
-                    const exists = response?.body.some(item => item.id == categoryCreated?.id)
+                    cy.log(JSON.stringify(response?.body))
+                    const exists = response?.body.some(item => item.id == archivedCategory?.id)
                     expect(exists).to.be.false
                     cy.get('.card').should('not.have.class', 'archived');
 
                     cy.get('#previous').click()
-                    cy.wait<CategoryReq, CategoryRes[]>('@getCategories')
 
                     cy.wait<CategoryReq, CategoryRes[]>('@getCategories').then(({ request, response }) => {
                       cy.log(JSON.stringify(response?.body))
-                      const exists2 = response?.body.some(item => item.id == categoryCreated?.id)
+                      const exists2 = response?.body.some(item => item.id == archivedCategory?.id)
                       expect(exists2).to.be.true
-                      const category = response?.body.find(item => item.id == categoryCreated?.id);
+                      const category = response?.body.find(item => item.id == archivedCategory?.id);
                       expect(category?.isArchived).to.be.true
                       cy.get('.card').should('have.class', 'archived');
                     })
