494dc8a53002b1209a630f10a25979256a46301b
NikNeiser <moshkovskiynikolay@mail.ru>
2025-10-14 01:06:51 +0300

    CreateMigration

---
+++ EasyFinance.Persistence/Migrations/20251013180056_ChangeExpenseItemsDeleteBehaviorToCascade.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EasyFinance.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeExpenseItemsDeleteBehaviorToCascade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExpenseItems_Expenses_ExpenseId",
+                table: "ExpenseItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExpenseItems_Expenses_ExpenseId",
+                table: "ExpenseItems",
+                column: "ExpenseId",
+                principalTable: "Expenses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ExpenseItems_Expenses_ExpenseId",
+                table: "ExpenseItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ExpenseItems_Expenses_ExpenseId",
+                table: "ExpenseItems",
+                column: "ExpenseId",
+                principalTable: "Expenses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
