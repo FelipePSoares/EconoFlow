575dfb41adacd1b20887ada3745131bd066b3768
Felipe Soares <>
2026-03-11 23:55:20 +0100

    hotfix for header

--- EasyFinance.Server/MiddleWare/SafeHeadersMiddleware.cs
+++ EasyFinance.Server/MiddleWare/SafeHeadersMiddleware.cs
@@ -8,6 +8,7 @@ namespace EasyFinance.Server.MiddleWare
             {
                 context.Response.Headers.Append("Referrer-Policy", "strict-origin-when-cross-origin");
                 context.Response.Headers.Append("X-Content-Type-Options", "nosniff");
+                context.Response.Headers.Append("X-Frame-Options", "DENY");
                 context.Response.Headers.Append("Permissions-Policy", "geolocation=(self), microphone=(), camera=()");
                 await next();
             });
